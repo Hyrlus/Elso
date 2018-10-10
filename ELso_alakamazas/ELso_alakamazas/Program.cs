a3ce9a50c94f315e52d01d3c0cd2072bfb0faa00
Hyrlus <ardai.mark20@gmail.com>
2018-10-10 16:16:05 +0200

    1.feladat megoldása

--- ELso_alakamazas/ELso_alakamazas/Program.cs
+++ ELso_alakamazas/ELso_alakamazas/Program.cs
@@ -8,8 +8,21 @@ namespace ELso_alakamazas
 {
     class Program
     {
+        public static int[] szamok = new int[10];
         static void Main(string[] args)
         {
+            
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.Write("Kérem adjon meg egy számot: ");
+                szamok[i] = Convert.ToInt32(Console.ReadLine());
+                
+                
+            }
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.Write(szamok[i] + " ");
+            }
         }
     }
 }
